995bd74b0d804906e85bcaef142b191e7ef034a4
MahonriM <cripto3000z@gmail.com>
2020-07-20 13:59:48 -0500

    Add files via upload

---
+++ Cookie.py
@@ -0,0 +1,12 @@
+from flask import Flask,make_response
+app=Flask(__name__)
+@app.route('/')
+
+def index():
+	response = make_response('<h1>Aqui esta la cookie</h1>')
+	response.set_cookie('answer','42')
+	return response
+if __name__=='__main__':
+	app.run(host='0.0.0.0')
+
+
